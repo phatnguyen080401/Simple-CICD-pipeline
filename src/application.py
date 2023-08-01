4f5880c1ebc456c494f2d7c13184d3cca5ba5eb0
Phat <19110101@student.hcmute.edu.vn>
2023-08-01 17:19:04 +0700

    Source code for web

---
+++ src/application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+@application.route("/")  
+def home():
+	return "This is the main page <h1>NICE TO MEET YOU</h1>"
+
+if __name__ == "__main__":
+  application.run(debug=True)
