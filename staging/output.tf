a7009e16d3cf2022ede40d4a4f9c1dfb59968746
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-08-02 11:07:40 +0700

    Retrieve S3 bucket url

--- staging/output.tf
+++ staging/output.tf
@@ -1,3 +1,15 @@
 output "domain-url" {
   value = module.ebs_staging.ebs_cname
 }
+
+output "s3-bucket-name" {
+  value = module.s3_staging.web_bucket.id
+}
+
+output "s3-bucket-object-name" {
+  value = module.s3_staging.web_bucket_object.id
+}
+
+output "s3-bucket-url" {
+  value = "s3://${module.s3_staging.web_bucket.id}/${module.s3_staging.web_bucket_object.id}"
+}
