9f1e28dbae8db22b865b129489216d799f480df1
phatnguyen080401 <phat.nguyen@c2c-techhub.io>
2023-08-02 15:15:13 +0700

    Change text in web

--- src/application.py
+++ src/application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")  
 def home():
-	return "This is the main page <h1>NICE TO MEET YOU</h1>"
+	return "This is the main page <h1>NICE TO MEET YOU AGAIN</h1>"
 
 if __name__ == "__main__":
   application.run(debug=True)
